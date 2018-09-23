f7668b78ee1cb8834682e75b933fddc403e0e336
murad <muradkv@gmail.com>
2018-09-24 01:38:15 +0300

    Initial LoginFormController

---
+++ WeatherApp/LoginFormController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginFormController.swift
+//  WeatherApp
+//
+//  Created by murad on 24.09.2018.
+//  Copyright © 2018 Murad. All rights reserved.
+//
+
+import UIKit
+
+class LoginFormController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
