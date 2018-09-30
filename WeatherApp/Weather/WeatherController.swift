b1e239434bb773cbea1529cad68aefa8705a726e
murad <muradkv@gmail.com>
2018-10-01 02:13:27 +0300

    Show weather on CollectionView
    Вывод градусов, даты и иконки изображения погоды в CollectionView выбранного города.

--- WeatherApp/Weather/WeatherController.swift
+++ WeatherApp/Weather/WeatherController.swift
@@ -8,37 +8,30 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
-
 class WeatherController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
     }
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return 10
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
-    
-        return cell
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WeatherCell", for: indexPath) as? WeatherCell {
+            cell.weather.text = "25 C"
+            cell.time.text = "1.10.2018"            
+            return cell
+        } else {
+            return UICollectionViewCell()
+        }
     }
 }
