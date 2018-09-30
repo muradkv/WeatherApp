9b2f72304e25a63462cd8d3b4df459a90db5c4c5
murad <muradkv@gmail.com>
2018-10-01 02:06:01 +0300

    Add Weather Controller and Cell
    Добавлен Controller и Cell для погоды на CollectionTableView, связали со сценой на сториборде.
    Добавлены два label(weather, time) и image(icon).

---
+++ WeatherApp/Weather/WeatherCell.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherCell.swift
+//  WeatherApp
+//
+//  Created by murad on 01/10/2018.
+//  Copyright © 2018 Murad. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UICollectionViewCell {
+    @IBOutlet weak var weather: UILabel!
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var time: UILabel!
+    
+}
