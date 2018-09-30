e183a2210422c09101e02b308384992149c94b5c
murad <muradkv@gmail.com>
2018-09-30 23:43:21 +0300

    Add AllCities Controller and Cell
    Создали Controller и Cell для выбора города(ов) на втором TableView, связали со сценой на сториборде.
    Добавили label cityName.

---
+++ WeatherApp/AllCities/AllCitiesCell.swift
@@ -0,0 +1,25 @@
+//
+//  AllCitiesCell.swift
+//  WeatherApp
+//
+//  Created by murad on 30/09/2018.
+//  Copyright © 2018 Murad. All rights reserved.
+//
+
+import UIKit
+
+class AllCitiesCell: UITableViewCell {
+    @IBOutlet weak var cityName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
